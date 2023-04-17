bca95d4a4b0caa445a84ec75d6f4944b4dc2f5b0
ilongp1984 <ilongp@naver.com>
2023-04-17 19:27:34 +0900

    EnemyAttack, PlayerController 스크립트 추가

---
+++ Assets/HJH/0000/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float maxHealth = 100f;      // 최대 체력
+    public float currentHealth = 100f;  // 현재 체력
+
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void TakeDamage(float damage)
+    {
+        currentHealth -= damage;
+        if (currentHealth <= 0)
+        {
+            // 사망 애니메이션 실행
+            animator.SetTrigger("Death");
+            // 이후, 다른 액션을 막기 위해 스크립트를 비활성화
+            enabled = false;
+        }
+        else
+        {
+            // 피격 애니메이션 실행
+            animator.SetTrigger("TakeDamage");
+        }
+    }
+}
