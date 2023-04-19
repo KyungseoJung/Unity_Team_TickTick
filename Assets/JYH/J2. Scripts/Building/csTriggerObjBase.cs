9098b6e77f7e5124e4074d5aa4440a3d1b0e508d
jyh94615 <jyh94615@gmail.com>
2023-04-19 21:46:04 +0900

    텐트 입구에서 순간이동 구현해봄

---
+++ Assets/JYH/J2. Scripts/Building/csTriggerObjBase.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class csTriggerObjBase : MonoBehaviour
+{
+    public Transform childPos;
+
+    public csTriggerObjBase _TOB;
+
+    public bool isUse = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player" && !isUse)
+        {
+            _TOB.isUse = true;
+            isUse = true;
+
+            _TOB.Invoke("EnableIsUse", 2f);
+            Invoke("EnableIsUse", 2f);
+
+            other.transform.position = childPos.position;
+        }
+    }
+
+    void EnableIsUse()
+    {
+        isUse = false;
+    }
+}
