19b2089d2b95904c79a1fc2892f2a0bfb535d963
jyh94615 <jyh94615@gmail.com>
2023-05-02 20:41:18 +0900

    헤딩 6

---
+++ Assets/JYH/J2. Scripts/csGameStart.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class csGameStart : MonoBehaviour
+{
+    private void Awake()
+    {
+        PhotonNetwork.Instantiate("MainGamePrefap", Vector3.zero, Quaternion.identity, 0);
+    }
+}
